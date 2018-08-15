9cb4637d6882bb34e7a84dddf9945785dddc2162
Romain Loeuillet <loeuillet.romain@gmail.com>
2018-08-15 16:53:04 +0200

    began airbnb

---
+++ app/controllers/flats_controller.rb
@@ -0,0 +1,23 @@
+class FlatsController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def index
+      @flats = Flat.all
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def delete
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,38 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Flat.destroy_all
+
+puts 'Creating flats...'
+Flat.create!
+flats_attributes = [
+  {
+    name: 'Garden Flat London',
+    address: '10 Clifton Gardens London W9 1DT',
+    description: 'Incredbible',
+    price_per_night: 75,
+    number_of_guests: 3
+  },
+  {
+    name: 'Loft London',
+    address: '6 Clifton Gardens London W9 1DT',
+    description: 'Spacious',
+    price_per_night: 50,
+    number_of_guests: 2
+  },
+  {
+    name: 'Apartment London',
+    address: '8 Clifton Gardens London W9 1DT',
+    description: 'Crazy',
+    price_per_night: 85,
+    number_of_guests: 6
+  },
+  {
+    name: 'House London',
+    address: '12 Clifton Gardens London W9 1DT',
+    description: 'Lovely',
+    price_per_night: 180,
+    number_of_guests: 10
+  }
+]
+
+Flat.create!(flats_attributes)
+puts 'Finished!'
